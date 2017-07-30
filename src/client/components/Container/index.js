21ca245d8068581d943062c82a194075906d7cab
Mehdi AISSA-BRAHIM <maissa-b@e3r4p21.42.fr>
2017-07-30 22:25:40 +0200

    Modification for overlay.

--- src/client/components/Container/index.js
+++ src/client/components/Container/index.js
@@ -5,9 +5,9 @@ import './container.css';
 import Header from '../Header';
 import MainPanel from '../MainPanel';
 
-const Container = ({ title, langs, resultItems }) => (
+const Container = ({ title, langs, resultItems, showOverlayPanel, isHidden }) => (
   <div className="container-container">
-    <Header title={title} langs={langs} />
+    <Header title={title} langs={langs} showOverlayPanel={showOverlayPanel} isHidden={isHidden} />
     <MainPanel resultItems={resultItems} />
   </div>
 );
@@ -16,6 +16,8 @@ Container.propTypes = {
   title: PropTypes.string.isRequired,
   langs: PropTypes.array.isRequired,
   resultItems: PropTypes.array.isRequired,
+  showOverlayPanel: PropTypes.func.isRequired,
+  isHidden: PropTypes.bool.isRequired,
 };
 
 export default Container;
--- src/client/components/Header/index.js
+++ src/client/components/Header/index.js
@@ -6,11 +6,11 @@ import SidePanelButton from '../SidePanelButton';
 import Title from '../Title';
 import LanguageSelector from '../LanguageSelector';
 
-const Header = ({ title, langs }) => (
+const Header = ({ title, langs, showOverlayPanel, isHidden }) => (
   <div>
     <nav className="pt-navbar pt-dark">
       <span className="pt-navbar-group pt-align-left">
-        <SidePanelButton />
+        <SidePanelButton isHidden={isHidden} showOverlayPanel={showOverlayPanel} />
       </span>
       <span className="pt-navbar-group">
         <Title title={title} />
@@ -25,6 +25,8 @@ const Header = ({ title, langs }) => (
 Header.propTypes = {
   title: PropTypes.string.isRequired,
   langs: PropTypes.array.isRequired,
+  showOverlayPanel: PropTypes.func.isRequired,
+  isHidden: PropTypes.bool.isRequired,
 };
 
 export default Header;
